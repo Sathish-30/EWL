9b8749e40e2911d5bc3939bfe0599b34a78fe276
Sathish-39 <sathish.amsaravanan@gmail.com>
2022-09-22 11:42:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const root = document.getElementById("root");
+const fetchinfo = () => {
+  const promises = [];
+  for (let i = 1; i <= 20; i++) {
+    const url = `https://randomuser.me/api/?results=${i}`;
+    //Fetched informations are pushed to promises
+    promises.push(fetch(url).then((res) => res.json()));
+  }
+
+  Promise.all(promises).then((results) => {
+    const personInfo = results.map((data) => ({
+      name: data.name.first,
+      email: data.email,
+      location: data.location.city,
+      image: data.picture.medium,
+    }));
+    displayInfo(personInfo);
+  });
+};
+const displayInfo = (personInfo) => {
+  console.log(personInfo);
+};
+
+fetchinfo();
