ad1b761027b9e9ff2db749e0dc391e49ea14599f
Sathish-39 <sathish.amsaravanan@gmail.com>
2022-09-22 15:19:26 +0530

    Commit 3

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const fetchinfo = fetch("https://randomuser.me/api/?results=10")
+const fetchinfo = fetch("https://randomuser.me/api/?results=20")
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    window.localStorage.setItem("user", JSON.stringify(data));
+  });
+console.log(JSON.parse(localStorage.getItem("user")));
+console.log(Object.keys(localStorage));
+console.log(Object.keys(localStorage).length);
