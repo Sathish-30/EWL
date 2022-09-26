7328054847fc4660450ff34f7c17b08075fea575
Sathish-39 <sathish.amsaravanan@gmail.com>
2022-09-26 22:05:21 +0530

    Few changes takes place

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const fetchinfo = fetch("https://randomuser.me/api/?results=20")
   .then((response) => response.json())
   .then((data) => {
-    window.localStorage.setItem("user", JSON.stringify(data));
+    //Local storage
+    localStorage.setItem("user", JSON.stringify(data.results));
+    const userProfile = JSON.parse(localStorage.getItem("user"));
+    console.log(userProfile);
   });
-const userProfile = JSON.parse(localStorage.getItem("user"));
-console.log(userProfile);
 
-const msgsender = document.getElementById("para");
+//Checking the element is present or not
+const prof = JSON.parse(localStorage.getItem("user"));
+console.log(prof[1].email);
+console.log(prof[1].login.password);
+
+//Form validation
+
+var mail = document.getElementById("email").value;
+var passWord = document.getElementById("pass").value;
+
 const formValidation = () => {
-  let mailId = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-  if (mailId == null || password == null) {
-    return false;
-  } else if (password < 6) {
-    return true;
+  var email = [];
+  var pass = [];
+  userProfile.forEach((data) => {
+    email.push(data.login.email);
+    pass.push(data.login.password);
+  });
+  // console.log(email);
+  // console.log(pass);
+  var ele = username.indexof(userName);
+  console.log(ele);
+  if (ele > 0) {
+    if (mail == email[ele] && passWord == pass[ele]) {
+      window.open("index.html");
+    } else {
+      document.getElementById("para").innerHTML = "Invalid ID";
+    }
   }
 };
